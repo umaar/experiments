7e4c3f87ae00815fe0a1b559b103c7fbf31edc53
Umar <umar.hansa@gmail.com>
2014-11-11 18:00:03 +0000

    Relative to available viewport size

--- particles/main.js
+++ particles/main.js
@@ -1,4 +1,4 @@
-(function(doc) {
+(function(win, doc) {
 
 	var canvas, width, height;
 	var particles = [];
@@ -26,18 +26,19 @@
 
 	function Particle() {
 		this.x = width/2;
-		this.vx = random(-10, 10);
-		this.y = height/2;
-		this.vy = random(-20, 1);
-		this.size = random(5, 10);
+		this.vx = random(-width, width);
+		this.y = height * 0.4;
+		this.vy = random( -(height * 0.05) ,1);
+		this.size = random(width * 0.009, width * 0.020);
+		this.colour = random(0, 360);
 		particles.push(this);
 	}
 
 	Particle.prototype.draw = function() {
-		canvas.fillStyle = 'white';
-		this.x += this.vx * 0.05;
-		this.y += this.vy * 0.05;
-		this.vy += 0.5;
+		canvas.fillStyle = 'hsla('+this.colour+', 50%, 50%, 0.4)';
+		this.x += this.vx * 0.005;
+		this.y += this.vy * 0.2;
+		this.vy += 0.3;
 		canvas.beginPath();
 		canvas.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
 		canvas.fill();
@@ -50,7 +51,7 @@
 	}
 
 	function start() {
-		width = height = 100;
+		width = height = Math.min(win.innerWidth, win.innerHeight) / 2;
 		canvas = createCanvas();
 		createparticles(particleAmount);
 		requestAnimationFrame(animate);
@@ -60,4 +61,4 @@
 		document.addEventListener("DOMContentLoaded", start, false);
 	}
 
-}(document));
+}(window, document));
