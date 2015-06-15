213dc9c3de8bdc4971a0b4b0df5aa577980860f8
Umar <umar.hansa@gmail.com>
2015-06-16 00:04:40 +0100

    dynamics

--- dynamics/js/main.js
+++ dynamics/js/main.js
@@ -1,8 +1,60 @@
 $(function() {
-	dynamics.animate($('.circle')[0], {
-		translateX: 350
-	}, {
-		type: dynamics.spring,
-		delay: 1000
-	});
+
+	function animate4() {
+		dynamics.animate($('.circle')[0], {
+			translateX: 0,
+			translateY: 0
+		}, {
+			type: dynamics.spring,
+			frequency: 274,
+			friction: 323,
+			anticipationSize: 59,
+			anticipationStrength: 62,
+		});
+	}
+
+	function animate3() {
+		dynamics.animate($('.circle')[0], {
+			translateX: 0,
+			translateY: 100
+		}, {
+			type: dynamics.spring,
+			frequency: 274,
+			friction: 323,
+			anticipationSize: 59,
+			anticipationStrength: 62,
+			complete: animate4
+		});
+	}
+
+	function animate2() {
+		dynamics.animate($('.circle')[0], {
+			translateX: 350,
+			translateY: 100
+		}, {
+			type: dynamics.spring,
+			frequency: 274,
+			friction: 323,
+			anticipationSize: 59,
+			anticipationStrength: 62,
+			complete: animate3
+		});
+	}
+
+	function start() {
+		dynamics.animate($('.circle')[0], {
+			translateX: 350
+		}, {
+			type: dynamics.spring,
+			frequency: 274,
+			friction: 323,
+			anticipationSize: 59,
+			anticipationStrength: 62,
+			delay: 400,
+			complete: animate2
+		});
+	}
+
+	start();
+
 });
