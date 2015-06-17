a4bd37c2c093df5e3d0f8fb41994c8eed78034da
Umar <umar.hansa@gmail.com>
2015-06-17 23:08:58 +0100

    dynamics

--- dynamics/js/main.js
+++ dynamics/js/main.js
@@ -1,5 +1,67 @@
 $(function() {
 
+	function animate12() {
+		dynamics.animate($('.circle')[3], {
+			translateY: 50,
+			opacity: 1
+		}, {
+			type: dynamics.gravity,
+			duration: 400,
+			 bounciness: 718,
+			 elasticity: 197
+		});
+	}
+
+	function animate11() {
+		dynamics.animate($('.circle')[2], {
+			translateY: 100,
+			opacity: 1
+		}, {
+			type: dynamics.gravity,
+			duration: 700,
+			 bounciness: 718,
+			 elasticity: 197,
+			 complete: animate12
+		});
+	}
+
+	function animate10() {
+		dynamics.animate($('.circle')[1], {
+			translateY: 150,
+			opacity: 1
+		}, {
+			type: dynamics.gravity,
+			duration: 700,
+			 bounciness: 718,
+			 elasticity: 197,
+			 complete: animate11
+		});
+	}
+
+	function animate9() {
+		dynamics.animate($('.circle')[0], {
+			translateY: 200
+		}, {
+			type: dynamics.gravity,
+			duration: 1300,
+			 bounciness: 718,
+			 elasticity: 197,
+			 complete: animate10
+		});
+	}
+
+	function animate8() {
+		dynamics.animate($('.container')[0], {
+			translateY: 200,
+			scale: 0.9
+		}, {
+			type: dynamics.gravity,
+			 bounciness: 418,
+			 elasticity: 197,
+			 complete: animate9
+		});
+	}
+
 	function animate7() {
 		dynamics.animate($('.container')[0], {
 			rotateZ: '180deg',
@@ -9,7 +71,8 @@ $(function() {
 			frequency: 274,
 			friction: 323,
 			anticipationSize: 59,
-			anticipationStrength: 62
+			anticipationStrength: 62,
+			complete: animate8
 		});
 	}
 
