17e37f6bb96642c6bb4e2795abc4da1afb265088
Umar <umar.hansa@gmail.com>
2015-08-12 17:56:05 +0100

    Force https

--- js-2015/js/filter.js
+++ js-2015/js/filter.js
@@ -5,4 +5,5 @@ export default (data) => data.data.children
 	.filter(post => !post.data.over_18)
 	.map(post => post.data.url)
 	.filter(url => url.match('.gif'))
-	.map(url => url.replace('.gifv', '.gif'));
+	.map(url => url.replace('.gifv', '.gif'))
+	.map(url => url.replace('http:', 'https:'));
