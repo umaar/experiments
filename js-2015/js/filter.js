004fac156869283aab40a561139b16dfd3cd7992
Umar <umar.hansa@gmail.com>
2016-03-21 16:58:28 +0000

    stuff

--- js-2015/js/filter.js
+++ js-2015/js/filter.js
@@ -2,6 +2,24 @@
 export default (data) => data.data.children
 	.filter(post => !post.data.over_18)
 	.map(post => post.data.url)
-	.filter(url => url.match('.gif'))
+
+
+if (this) {
+	console.log('hi')
+	if (this && window) {
+		console.log('yeah')
+		console.log('yeah')
+	}
+}
+
+function fetchit() {
+	fetch();
+}
+
+
+var regexp = '.gif';
+filter(url => url.match(regexp))
 	.map(url => url.replace('.gifv', '.gif'))
+	.map(url => url.replace('http:', 'https:'));
+	.map(url => url.replace('http:', 'https:'));
 	.map(url => url.replace('http:', 'https:'));
