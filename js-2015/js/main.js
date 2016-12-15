7cdd9e3159ef3ee520e95cac2594527556abd87b
Umar Hansa <umar.hansa@gmail.com>
2016-12-15 14:22:06 +0000

    fix

--- js-2015/js/main.js
+++ js-2015/js/main.js
@@ -3,7 +3,5 @@ import reddit from './reddit';
 import filter from './filter';
 import display from './display';
 
-
-var onResolve = display;
 reddit().then(filter).
-then(onResolve);
+then(display);
